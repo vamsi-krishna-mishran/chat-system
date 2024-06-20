10ed2dd89bde511dbd262f95a503802804565c6d
vamsi_analinear <vamsi@analinear.com>
2024-06-20 18:26:16 +0530

    updated chat route

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,12 +1,50 @@
 import './chat.css'
+import { SendOutlined, FileAddOutlined } from '@ant-design/icons'
 
-function Chat() {
+function Chat()
+{
   return (
     <div className='chat'>
-        <h3>Messages</h3>
-        <hr/>
+      <h3>Messages</h3>
+      <hr />
+      <div className='entry-msg'>
+        <FileAddOutlined />
+        <input type='text' placeholder='message here'></input>
+        <SendOutlined />
+
+      </div>
+      <div className='render-msg'>
+        <ReceiveMessage />
+        <SendMessage />
+        <ReceiveMessage />
+        <SendMessage />
+        <ReceiveMessage />
+        <SendMessage />
+        <ReceiveMessage />
+        <SendMessage />
+        <SendMessage />
+        <ReceiveMessage />
+        {/* <SendMessage />
+        <ReceiveMessage />
+        <SendMessage />
+       */}
+      </div>
     </div>
   )
+}
+
+const ReceiveMessage = () =>
+{
+  return <div className='receive-message'>
+    Hi bro.
+  </div>
+}
+
+const SendMessage = () =>
+{
+  return <div className='send-message'>
+    Hi, how are you?
+  </div>
 }
 
 export default Chat
