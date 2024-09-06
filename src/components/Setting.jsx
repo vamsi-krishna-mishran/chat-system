1f83989bc0b37206c608b5da826960786b571b21
vamsi_analinear <vamsi@analinear.com>
2024-09-06 19:28:00 +0530

    settings updated

--- src/components/Setting.jsx
+++ src/components/Setting.jsx
@@ -2,13 +2,17 @@ import React, { useState } from "react";
 import './profileComp.css'
 import './myAccount.css'
 import { Select } from 'antd'
+import { UserAddOutlined } from '@ant-design/icons'
 
 const options2 = ['None', 'Allow Everyone']
-
+const allUsers = [{ name: 'vamsi', id: 1 }, { name: 'krshna', id: 2 }, { name: 'mani', id: 3 }
+    , { name: 'vamsi', id: 4 }, { name: 'krshna', id: 5 }, { name: 'mani', id: 6 }
+]
 
 const SettingComp = () =>
 {
     const [options, setOptions] = useState([options2[0], options2[0], options2[0]])//profile,lastseen,online.
+    const [blockedUsers, setBlockedUsers] = useState(allUsers)
     console.log(options)
     const changeProfile = (e) =>
     {
@@ -28,6 +32,10 @@ const SettingComp = () =>
         let clone = [...options]
         clone[2] = e
         setOptions((prev) => clone)
+    }
+    const unBlockUser = (user2) =>
+    {
+        setBlockedUsers((prev) => prev.filter(user => user.id != user2.id))
     }
     return (
         <div className="setting-comp">
@@ -77,6 +85,30 @@ const SettingComp = () =>
                         value: province,
                     }))}
                 />
+            </div>
+            <div className='option'>
+                <div className='label-add'>
+                    <label>Blocked Users</label>
+                    <UserAddOutlined title='block a user' style={{ fontSize: '1.5rem', cursor: 'pointer' }} />
+                </div>
+                <div className="blocked-users-wrapper">
+                    {blockedUsers.map((user) => <UserComponent unBlock={() => unBlockUser(user)} user={user} />)}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+const UserComponent = ({ user, unBlock }) =>
+{
+
+    return (
+        <div className='user-wrapper'>
+            <div className='name-wrapper'>
+                {user.name}
+            </div>
+            <div className='unblock-wrapper'>
+                <button onClick={unBlock} title='unblock user'>unblock</button>
             </div>
         </div>
     )
